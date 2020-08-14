3fb681d44e323275309cfcacb80609f14df90c2f
Limore <1300803599@qq.com>
2020-08-14 17:33:01 +0800

    第二次合并

---
+++ main/get_data_daily.py
@@ -0,0 +1,32 @@
+import baostock as bs
+import pandas as pd
+
+def get_data_d():
+    ####登录系统####
+    lg = bs.login()
+    #显示登录返回信息
+    print('login respond error_code:'+lg.error_code)
+    print('login respond error_msg:'+lg.error_msg)
+
+    #####获取股票历史K线数据####
+    #详细指标参数
+
+    stock_name = "sz.002739"
+
+    rs = bs.query_history_k_data_plus(stock_name, "date,open,high,low,close,volume,amount,preclose,pctChg", start_date='2020-01-01', end_date='2020-08-10', frequency="d")
+    print('query_history_k_data_plus respond error_code:'+rs.error_code)
+    print('query_history_k_data_plus error_msg:'+rs.error_msg)
+
+    ####打印结果集####
+    data_list = []
+    while (rs.error_code == '0') & rs.next():
+        #获取一条记录，将记录合并在一起
+        data_list.append(rs.get_row_data())
+    result = pd.DataFrame(data_list, columns=rs.fields)
+
+    ####结果输出到csv文件####
+    result.to_csv("stock_k_data.csv", index=False)
+    print(result)
+
+    ####登出系统####
+    bs.logout
---
+++ 因数计算_严政_U201811950/main.py
@@ -0,0 +1,19 @@
+import baostock as bs
+import pandas as pd
+
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+import data
+import calculate
+import get_data_daily
+
+
+# 获取数据
+get_data_daily.get_data_d()
+
+# 画图
+data.plot_pic()
+
+# 计算指数
+calculate.calculate()
